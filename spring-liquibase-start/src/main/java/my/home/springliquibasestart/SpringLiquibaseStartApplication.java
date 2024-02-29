7528d2988fdd00a25d26af0984df61b763558320
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-02-29 19:26:26 +0300

    init repo

---
+++ spring-liquibase-start/src/main/java/my/home/springliquibasestart/SpringLiquibaseStartApplication.java
@@ -0,0 +1,13 @@
+package my.home.springliquibasestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseStartApplication.class, args);
+    }
+
+}
---
+++ spring-liquibase-start/src/test/java/my/home/springliquibasestart/SpringLiquibaseStartApplicationTests.java
@@ -0,0 +1,13 @@
+package my.home.springliquibasestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
